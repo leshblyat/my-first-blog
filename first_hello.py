4a034c47d1efea404c9391ee12bbb5df86201627
Lesh <marenkoz.a@gmail.com>
2019-01-19 14:01:01 +0300

    My Django Girls app, first commit

---
+++ first_hello.py
@@ -0,0 +1,30 @@
+#name = "Lehis"
+#if name == "Lexis":
+#    print("Hello, Lexis")
+#elif name == "Lehis":
+#    print("Hello, Lehis")
+#else:
+#    print("Goodbye")
+
+# Change the volume if it's too loud or too quiet
+#volume = 50
+#if volume > 20 or volume < 80:
+#    print("That's better!")
+#def hi(name):
+#    if name == "Lexis":
+#        print("Hello, Lexis")
+#    elif name == "Lehis":
+#        print("Hello, Lehis")
+#    else:
+#        print("Goodbye")
+#hi("poop")
+def hi(name):
+    print("Hello, " + name + "!")
+#hi("Lehis")
+
+boys = ["Alehis", "Miha", "Sasha", "Kirill", "Sam"]
+for name in boys:
+    hi(name)
+    print("Next bro")
+for i in range(1, 7):
+    print(i)
